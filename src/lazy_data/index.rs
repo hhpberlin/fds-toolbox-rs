4716bdabe1391fba3d2bb5e473da8349d2a9d2ee
Nikolas Kilian <n.kilian@hhpberlin.de>
2022-07-19 07:28:24 +0200

    Even more index

--- src/lazy_data/index.rs
+++ src/lazy_data/index.rs
@@ -1,53 +1,63 @@
-use std::{sync::{RwLock, Arc}, future::Future, error::Error, pin::Pin};
+use std::{sync::{Arc}, future::Future, error::Error, pin::Pin, time::Instant};
 
 use dashmap::DashMap;
 
 use super::serialization::{Data, Serializer};
 
-type BoxFuture<T, E = Box<dyn Error>> = Box<dyn Future<Output = Result<T, E>> + Unpin>;
+// use tokio::prelude::*;
 
-pub enum CASValue {
+type BoxFuture<'a, T, E = Box<dyn Error + 'a>> = Pin<Box<dyn Future<Output = Result<T, E>> + 'a>>;
+
+pub enum CASValue<'a> {
     Data(Arc<dyn Data>),
-    FutureData(BoxFuture<Box<dyn Data>>),
+    FutureData(BoxFuture<'a, Arc<dyn Data + 'a>>),
     Compressed(Vec<u8>),
-    FutureCompressed(BoxFuture<Vec<u8>>),
+    FutureCompressed(BoxFuture<'a, Vec<u8>>),
 }
 
 pub struct CASIndex<'a> {
-    stored: DashMap<&'a [u8], CASValue>,
+    stored: DashMap<&'a [u8], CASValue<'a>>,
+    last_uses: DashMap<&'a [u8], Instant>,
     serializer: Box<dyn Serializer>,
 }
 
-impl CASIndex {
+impl<'a> CASIndex<'a> {
     pub fn new(serializer: Box<dyn Serializer>) -> Self {
         Self {
             stored: DashMap::new(),
+            last_uses: DashMap::new(),
             serializer
         }
     }
 
-    pub async fn fetch<'a>(&'a self, key: &[u8]) -> Result<Arc<dyn Data + 'a>, Box<dyn Error + 'a>> {
-        let mut entry = self.stored.entry(key);
-        match entry {
-            dashmap::mapref::entry::Entry::Occupied(mut entryContent) => {
-                let val = entryContent.get()?;
-                match &*val {
+    pub async fn fetch(&'a self, key: &[u8]) -> Result<Arc<dyn Data + 'a>, Box<dyn Error + 'a>> {
+        match self.stored.get(key) {
+            Some(val) => {
+                match val.value() {
                     CASValue::Data(ref data) => Ok(data.clone()), // Only the Arc is cloned
                     _ => {
                         drop(val);
                         // let a = entryContent.get_mut().get_mut()?;
-                        self.stored.alter(key, |x| CASValue::Data(self.to_data(x)));
+                        let mut result;
+                        self.stored.alter(key, |_, x| {
+                            let data = self.to_data(x);
+                            let data: BoxFuture<'a, Arc<dyn Data + 'a>> = Box::pin(data);
+                            result = &data.shared();
+                            let out = CASValue::<'a>::FutureData(data);
+                            out
+                        });
+                        Ok(result.as_mut().await?)
                     }
                 }
             },
-            dashmap::mapref::entry::Entry::Vacant(val) => Err(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, "Key not found"))),
+            None => Err(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, "Key not found"))),
             // Some(value) => {
             // },
             // None => Err(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, "Key not found"))),
         }
     }
 
-    async fn to_data<'a>(&'a self, val: CASValue) -> Result<Arc<dyn Data + 'a>, Box<dyn Error + 'a>> {
+    async fn to_data(&'a self, val: CASValue<'a>) -> Result<Arc<dyn Data + 'a>, Box<dyn Error + 'a>> {
         match val {
             CASValue::Data(data) => Ok(data),
             CASValue::FutureData(future) => Ok(future.await?.into()),
