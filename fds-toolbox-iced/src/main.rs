3887798151d0aedac68a4c4a045bb1fbdafdb37e
blenderfreaky <blenderfreaky@mailbox.org>
2022-08-06 00:58:33 +0200

    Move stuff

---
+++ fds-toolbox-iced/src/main.rs
@@ -0,0 +1,34 @@
+use iced::{Sandbox, Settings};
+
+mod pane;
+
+pub fn main() -> iced::Result {
+    // MainWindow::run(Settings::default())
+    pane::main()
+}
+
+// struct MainWindow {
+
+// }
+
+// impl Sandbox for MainWindow {
+//     fn new() -> Self {
+//         MainWindow {
+
+//         }
+//     }
+
+//     fn title(&self) -> String {
+//         format!("{} - FDS Toolbox", "..")
+//     }
+
+//     type Message = ();
+
+//     fn update(&mut self, message: Self::Message) {
+    
+//     }
+
+//     fn view(&mut self) -> iced::Element<'_, Self::Message> {
+//         let mut controls = Row::new();
+//     }
+// }
