1f4fb7c525ff43f3c56f4eaf31e4ba9caafca25d
Doge815 <815.doge@gmail.com>
2022-11-12 23:35:22 +0100

    1 modified

--- fds-toolbox-ui/iced/src/select_list.rs
+++ fds-toolbox-ui/iced/src/select_list.rs
@@ -1,8 +1,11 @@
+use iced::{Element};
+use iced::widget::Row;
+
 fn view<'a, Message: Copy + 'a>(
-    children: impl Iterator<Item = (impl Fn(bool) -> pure::Element<'a, Message>, bool)>,
-) -> pure::Element<'a, Message> {
+    children: impl Iterator<Item = (impl Fn(bool) -> Element<'a, Message>, bool)>,
+) -> Element<'a, Message> {
     children
-        .fold(pure::row(), |row, (child, selected)| {
+        .fold(Row::new(), |row, (child, selected)| {
             row.push(child(selected))
         })
         .into()
