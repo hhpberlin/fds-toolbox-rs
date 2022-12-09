c45f6135fac6c843bc7b1780d68613c6fe9c0182
Nikolas Kilian <n.kilian@hhpberlin.de>
2022-12-09 12:10:48 +0100

    comment

--- fds-toolbox-core/src/formats/fds/fds_slice_frame.rs
+++ fds-toolbox-core/src/formats/fds/fds_slice_frame.rs
@@ -13,25 +13,25 @@ pub struct FdsSliceFrame {
     max_value: f32,
 }
 
-impl FdsSliceFrame {
-    fn new(reader: &mut impl Read, slice: FdsSlice, block: i32) -> Result<FdsSliceFrame, &str> {
-        let mut ret: FdsSliceFrame = FdsSliceFrame {
-            time: Time::new::<second>(reader.read_f32()),
-            values: vec![],
-            min_value: f32::MAX,
-            max_value: f32::MIN,
-        };
-        let _ = reader.skip(1);
+// impl FdsSliceFrame {
+//     fn new(reader: &mut impl Read, slice: FdsSlice, block: i32) -> Result<FdsSliceFrame, &str> {
+//         let mut ret: FdsSliceFrame = FdsSliceFrame {
+//             time: Time::new::<second>(reader.read_f32()),
+//             values: vec![],
+//             min_value: f32::MAX,
+//             max_value: f32::MIN,
+//         };
+//         let _ = reader.skip(1);
 
-        let block_size = reader.read_i32();
-        match block_size {
-            None => return Err("What is dis"),
-            Some(blk) => {
-                if block * 4 != blk {
-                    return Err("bad block");
-                }
-            }
-        }
-        Ok(ret)
-    }
-}
+//         let block_size = reader.read_i32();
+//         match block_size {
+//             None => return Err("What is dis"),
+//             Some(blk) => {
+//                 if block * 4 != blk {
+//                     return Err("bad block");
+//                 }
+//             }
+//         }
+//         Ok(ret)
+//     }
+// }
