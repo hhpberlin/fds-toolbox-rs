71f820ebdde41a1cf6da88636b63fd630f77f0a3
Doge815 <815.doge@gmail.com>
2022-12-11 23:56:34 +0100

    asdf

--- fds-toolbox-core/src/formats/slice/slice.rs
+++ fds-toolbox-core/src/formats/slice/slice.rs
@@ -1,11 +1,11 @@
 use crate::common::series::Series2;
-use crate::formats::slcf::slice_frame::FdsSliceFrame;
+use crate::formats::slice::slice_frame::SliceFrame;
 use crate::geom::bounds3int::{Bounds3I, Dimension3D};
 
 pub struct Slice {
     min_value: f32,
     max_value: f32,
-    frames: Series2<FdsSliceFrame>,
+    frames: Series2,
     bounds: Bounds3I,
     flat_dimension: Dimension3D,
     flat_dimension_position: i32,
@@ -14,4 +14,11 @@ pub struct Slice {
     quantity: String,
     short_name: String,
     units: String,
+}
+
+impl Slice {
+    fn new(reader: impl Reder) -> Slice
+    {
+        Slice { min_value: (), max_value: (), frames: (), bounds: (), flat_dimension: (), flat_dimension_position: (), dimension_i: (), dimension_j: (), quantity: (), short_name: (), units: () }
+    }
 }
