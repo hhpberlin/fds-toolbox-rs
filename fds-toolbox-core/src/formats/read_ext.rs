a08c7fed724efcb7996929a208059a93b9459018
blenderfreaky <blenderfreaky@mailbox.org>
2022-12-17 03:00:26 +0100

    add vlq reading

--- fds-toolbox-core/src/formats/read_ext.rs
+++ fds-toolbox-core/src/formats/read_ext.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use std::{io::{self, Read}, fmt::UpperHex};
 
 use byteorder::ReadBytesExt;
 use thiserror::Error;
@@ -15,19 +15,35 @@ pub enum ReadStrErr {
 
 pub trait ReadExt {
     fn read_string(&mut self) -> Result<String, ReadStrErr>;
+    fn read_vlq_u32(&mut self) -> Result<u32, io::Error>;
     fn skip(&mut self, n: usize) -> Result<(), io::Error>;
 }
 
 impl<T: Read> ReadExt for T {
     fn read_string(&mut self) -> Result<String, ReadStrErr> {
         let len = self
-            .read_u32::<byteorder::BigEndian>()?;
+            .read_vlq_u32()?;
         let len = len as usize;
 
         let mut buf: Vec<u8> = vec![0u8; len];
         self.read_exact(&mut buf).map_err(|err| ReadStrErr::IoBuf(err, len))?;
         
         Ok(String::from_utf8(buf)?)
+    }
+
+    fn read_vlq_u32(&mut self) -> Result<u32, io::Error> {
+        // TODO: Report overflows
+        let mut result = 0;
+        let mut shift = 0;
+        loop {
+            let b = self.read_u8()?;
+            result |= ((b & !(1u8 << 7)) as u32) << shift;
+            if b & (1u8 << 7) == 0 {
+                break;
+            }
+            shift += 7;
+        }
+        Ok(result)
     }
 
     fn skip(&mut self, n: usize) -> Result<(), io::Error> {
