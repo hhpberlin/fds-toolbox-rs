809002e16e6b7fe17099c595874db0beb465f022
Nikolas Kilian <n.kilian@hhpberlin.de>
2023-02-10 14:42:19 +0100

    more parsing

--- core/src/formats/smv.rs
+++ core/src/formats/smv.rs
@@ -1,9 +1,10 @@
 //pub type Input<'a> = &'a [u8];
 //pub type Result<'a, T> = nom::IResult<Input<'a>, T, ()>;
 
-use std::{collections::HashMap, io::Read};
+use std::{collections::HashMap, io::Read, num::ParseIntError, str::FromStr};
 
 use nom_locate::LocatedSpan;
+use thiserror::Error;
 
 type Span<'a> = LocatedSpan<&'a str>;
 
@@ -18,12 +19,25 @@ struct Simulation {
     solid_ht3d: f64, // TODO: Float or int?
 }
 
-enum SimulationParsingError {
-    UnexpectedWhitespace(Span<'static>),
+#[derive(Debug, Error)]
+enum Error<'a> {
+    #[error("Unexpected whitespace, expected no whitespace at the start of the line: {0}")]
+    UnexpectedWhitespace(Span<'a>),
+    #[error("Missing line after: {0}")]
+    MissingLine(Span<'a>),
+    #[error("Failed to parse invalid number: {0}")]
+    InvalidInt(Span<'a>, ParseIntError),
+}
+
+fn parse<'a, T: FromStr<Err = SourceErr>, SourceErr: Into<FnInErr>, FnInErr, FnOutErr: Into<TargetErr>, TargetErr>(
+    i: Span<'a>,
+    f: impl FnOnce(Span<'a>, FnInErr) -> TargetErr,
+) -> Result<T, TargetErr> {
+    i.fragment().parse().map_err(|x: SourceErr| f(i, x.into()).into())
 }
 
 impl Simulation {
-    pub fn parse(lines: impl Iterator<Item = &str>) -> Result<Self, ()> {
+    pub fn parse<'a>(lines: impl Iterator<Item = Span<'a>>) -> Result<Self, Error<'a>> {
         let mut title = None;
         let mut fds_version = None;
         let mut end_file = None;
@@ -31,22 +45,44 @@ impl Simulation {
         let mut revision = None;
         let mut chid = None;
         let mut csv_files = HashMap::new();
-        let mut solid_ht3d = None;
+        let mut solid_ht3d: Option<i32> = None; // TODO: Type
+        let mut num_meshes: Option<u32> = None;
 
-        for line in lines {
+        for line in lines.filter(|x| !x.trim_start().is_empty()) {
             // let line = line.trim_end();
             if line.trim_start().len() != line.len() {
-                return Err(());
+                return Err(Error::UnexpectedWhitespace(line));
             }
 
             let next = || {
-                lines.next().ok_or(())?.trim()
+                lines
+                    .next()
+                    .ok_or_else(|| Error::MissingLine(line))
+                    .map(|x| x. x.trim())
             };
 
-            match line {
-                "TITLE" => title = Some(next),
-                "FDS VERSION" => fds_version = Some(line),
+            match *line.fragment() {
+                "TITLE" => title = Some(next()?),
+                "FDSVERSION" => fds_version = Some(next()?),
+                "ENDF" => end_file = Some(next()?),
+                "INPF" => input_file = Some(next()?),
+                "REVISION" => revision = Some(next()?),
+                "CHID" => chid = Some(next()?),
+                "SOLID_HT3D" => {
+                    solid_ht3d = Some(parse(next()?, Error::InvalidInt)?)
+                }
+                "CSVF" => {
+                    // TODO
+                    let name = next()?;
+                    let file = next()?;
+                    csv_files.insert(name, file);
+                }
+                "NMESHES" => {
+                    num_meshes = Some(next()?.parse().map_err(|x| Error::InvalidInt(x))?)
+                }
             }
         }
+
+        Ok(())
     }
 }
