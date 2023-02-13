9be52a0ccb5d35011553206e37db5d90623acd58
blenderfreaky <blenderfreaky@mailbox.org>
2023-02-14 00:30:34 +0100

    even more smv

--- core/src/formats/smv/mod.rs
+++ core/src/formats/smv/mod.rs
@@ -8,7 +8,7 @@ use std::{
 
 use nom::{
     bytes::complete::{is_not, tag},
-    combinator::{map, map_res, opt},
+    combinator::{map, map_res, opt, success},
     sequence::tuple,
     IResult, Parser,
 };
@@ -39,6 +39,48 @@ struct Obst {
     texture_origin: Vec3F,
     // TODO: Map to actual surface type
     side_surfaces: Surfaces3<i32>,
+    bounds_idx: Bounds3I,
+    // TODO: Introduce rgb struct?
+    rgb: Option<Vec3F>,
+    color_index: i32,
+    block_type: i32,
+}
+
+#[derive(Debug)]
+struct Vent {
+    bounds: Bounds3F,
+    vent_index: i32,
+    surface: i32,
+    texture_origin: Option<Vec3F>,
+    bounds_idx: Bounds3I,
+    color_index: i32,
+    draw_type: i32,
+    // TODO: Introduce 4D vector or rgba struct?
+    rgba: Option<(Vec3F, f32)>,
+}
+
+#[derive(Debug)]
+struct CircularVent {
+    bounds: Bounds3F,
+    vent_index: i32,
+    surface: i32,
+    origin: Vec3F,
+    radius: f32,
+    bounds_idx: Bounds3I,
+    color_index: i32,
+    draw_type: i32,
+    rgba: Option<(Vec3F, f32)>,
+}
+
+#[derive(Debug)]
+struct Mesh {
+    name: String,
+    dimensions: Vec3U,
+    bounds: Bounds3F,
+    obsts: Vec<Obst>,
+    trn: Vec3<Vec<f32>>,
+    vents: Vec<Vent>,
+    circular_vents: Vec<CircularVent>,
 }
 
 enum Error<'a> {
@@ -69,11 +111,29 @@ enum ErrorKind {
     InvalidInt(ParseIntError),
     InvalidFloat(ParseFloatError),
     // TODO: Expected is currently a lower bound, not an exact value because of the way the macro is written
-    WrongNumberOfValues { expected: usize, got: usize },
+    WrongNumberOfValues {
+        expected: usize,
+        got: usize,
+    },
     TrailingCharacters,
     UnknownSection,
-    MismatchedIndex { expected: usize, got: usize },
-    UnexpectedMeshIdSign(i32),
+    MismatchedIndex {
+        expected: usize,
+        got: usize,
+    },
+    // An obst with an id of 0 was given (or .signum() returned a value other than -1, 0, or 1)
+    UnexpectedObstIdSign(i32),
+    // Rgb should only be given iff color_index == -3
+    InvalidObstColor {
+        color_index: i32,
+        rgb: Option<Vec3F>,
+    },
+    InvalidVentTextureOrigin {
+        i: usize,
+        num_vents: usize,
+        num_dummies: usize,
+        texture_origin: Option<Vec3F>,
+    },
 }
 
 fn err(pos: &str, kind: ErrorKind) -> Error {
@@ -296,7 +356,6 @@ impl Simulation {
                 }
                 "OFFSET" => offset = Some(parse!(next()? => vec3f)?),
                 line if line.starts_with("GRID") => {
-
                     todo!();
                 }
                 _ => return Err(err(line, ErrorKind::UnknownSection)),
@@ -313,51 +372,54 @@ impl Simulation {
             solid_ht3d: solid_ht3d.ok_or(Error::MissingSection { name: "SOLID_HT3D" })?,
         })
     }
+}
 
-    /// Checks if the current line matches the given tag or returns a fitting error
-    /// 
-    /// # Arguments
-    /// 
-    /// * `header` - The header of the current section, used for error messages
-    /// * `next` - The next function to get the next line
-    /// * `tag` - The tag to match
-    fn parse_subsection<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
-        header: &'a str,
-        mut next: Src,
-        tag: &'static str,
-    ) -> Result<(), Error<'a>> {
-        let err = Error::MissingSubSection {
-            parent: header,
-            name: tag,
-        };
-        match next() {
-            Ok(next_line) => match_tag(next_line, tag, err),
-            Err(_) => Err(err),
-        }
+/// Checks if the current line matches the given tag or returns a fitting error
+///
+/// # Arguments
+///
+/// * `header` - The header of the current section, used for error messages
+/// * `next` - The next function to get the next line
+/// * `tag` - The tag to match
+fn parse_subsection_hdr<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
+    header: &'a str,
+    mut next: Src,
+    tag: &'static str,
+) -> Result<(), Error<'a>> {
+    let err = Error::MissingSubSection {
+        parent: header,
+        name: tag,
+    };
+    match next() {
+        Ok(next_line) => match_tag(next_line, tag, err),
+        Err(_) => Err(err),
     }
+}
 
-    fn parse_mesh<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
-        header: &'a str,
-        default_texture_origin: Vec3F,
-        mut next: Src,
-    ) -> Result<(), Error<'a>> {
-        let (_, _mesh_name) = parse!(header => "GRID" full_line_string)?;
-        let (dimensions, _a) = parse!(next()? => vec3u i32)?;
+fn parse_mesh<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
+    header: &'a str,
+    default_texture_origin: Vec3F,
+    mut next: Src,
+) -> Result<Mesh, Error<'a>> {
+    let (_, mesh_name) = parse!(header => "GRID" full_line_string)?;
+    let (dimensions, _a) = parse!(next()? => vec3u i32)?;
 
-        Self::parse_subsection(header, &mut next, "PDIM")?;
-        let (bounds, _something) = parse!(next()? => bounds3f vec2f)?;
+    parse_subsection_hdr(header, &mut next, "PDIM")?;
+    let (bounds, _something) = parse!(next()? => bounds3f vec2f)?;
 
-        let parse_trn = |mut next: &mut Src, dim: Dim3D| {
-            // TODO: I'm not too fond of hardcoding the dimension names like this
-            Self::parse_subsection(header, &mut next, ["TRNX", "TRNY", "TRNZ"][dim as usize])?;
+    let parse_trn = |mut next: &mut Src, dim: Dim3D| {
+        // TODO: I'm not too fond of hardcoding the dimension names like this
+        parse_subsection_hdr(header, &mut next, ["TRNX", "TRNY", "TRNZ"][dim as usize])?;
 
-            // TODO: Why is this a thing? This is just copied from fdsreader right now but idk why it's there
-            let n = parse!(next()? => usize)?;
-            for _ in 0..n {
-                let _ = next()?;
-            }
+        // TODO: Why is this a thing? This is just copied from fdsreader right now but idk why it's there
+        let n = parse!(next()? => usize)?;
+        for _ in 0..n {
+            let _ = next()?;
+        }
 
-            repeat_n(&mut next, |next, line| {
+        repeat_n(
+            &mut next,
+            |next, line| {
                 let next = next()?;
                 let (i, v) = parse!(next => usize f32)?;
                 if i != line {
@@ -370,17 +432,68 @@ impl Simulation {
                     ));
                 }
                 Ok(v)
-            }, dimensions[dim] as usize)
-        };
+            },
+            dimensions[dim] as usize,
+        )
+    };
 
-        let trn = Vec3::new(
-            parse_trn(&mut next, Dim3D::X)?,
-            parse_trn(&mut next, Dim3D::Y)?,
-            parse_trn(&mut next, Dim3D::Z)?,
-        );
+    let trn = Vec3::new(
+        parse_trn(&mut next, Dim3D::X)?,
+        parse_trn(&mut next, Dim3D::Y)?,
+        parse_trn(&mut next, Dim3D::Z)?,
+    );
 
-        Self::parse_subsection(header, &mut next, "OBST")?;
-        let obsts = repeat(&mut next, |next, _| {
+    parse_subsection_hdr(header, &mut next, "OBST")?;
+    let obsts = parse_obsts(&mut next, default_texture_origin)?;
+
+    parse_subsection_hdr(header, &mut next, "VENT")?;
+    let vents = parse_vents(&mut next)?;
+
+    parse_subsection_hdr(header, &mut next, "CVENT")?;
+    let circular_vents = parse_circular_vents(&mut next)?;
+
+    // TODO: fdsreader doesn't parse this, but it's in the .smv file I'm referencing
+    parse_subsection_hdr(header, &mut next, "OFFSET")?;
+    let _offset = parse!(next()? => vec3f)?;
+
+    Ok(Mesh {
+        name: mesh_name,
+        dimensions,
+        bounds,
+        trn,
+        obsts,
+        vents,
+        circular_vents,
+    })
+}
+
+fn parse_obsts<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
+    mut next: Src,
+    default_texture_origin: Vec3<f32>,
+) -> Result<Vec<Obst>, Error<'a>> {
+    // Stores obstacles as they are defined in the first half
+    // Since obstacles are defined like this:
+    //
+    // OBST
+    //  2        number of obstacles
+    //  1.2 ...  obstacle 1
+    //  2.3 ...  obstacle 2
+    //  1 2 ...  more info about obstacle 1
+    //  3 4 ...  more info about obstacle 2
+    //
+    struct HalfObst {
+        name: Option<String>,
+        id: u32,
+        is_hole: bool,
+        bounds: Bounds3F,
+        texture_origin: Vec3F,
+        // TODO: Map to actual surface type
+        side_surfaces: Surfaces3<i32>,
+    }
+    let num_obsts = parse!(next()? => usize)?;
+    let obsts = repeat_n(
+        &mut next,
+        |next, _| {
             let next = next()?;
 
             // The id is signed, but the sign only represents if it's a hole or not
@@ -391,7 +504,7 @@ impl Simulation {
                 _ => Err(err(
                     // TODO: I don't like this, spans should be tracked more nicely
                     next.split_whitespace().nth(6).unwrap_or(next),
-                    ErrorKind::UnexpectedMeshIdSign(x),
+                    ErrorKind::UnexpectedObstIdSign(x),
                 )),
             });
 
@@ -402,10 +515,12 @@ impl Simulation {
             // TODO: As per the TODO above, should this be a global offset or the default value?
             let texture_origin = map(opt(vec3f), |x| x.unwrap_or(default_texture_origin));
 
+            // Full line looks like this:
+            // bounds3f (6xf32) id (i32) surfaces3i (6xi32) optional[texture_origin (3xf32)] optional[name (string)]
             let (bounds, (is_hole, id), side_surfaces, texture_origin, name) =
                 parse!(next => bounds3f id surfaces3i texture_origin name)?;
 
-            Ok(Obst {
+            Ok(HalfObst {
                 bounds,
                 is_hole,
                 id,
@@ -413,8 +528,165 @@ impl Simulation {
                 texture_origin,
                 name,
             })
-        });
+        },
+        num_obsts,
+    )?;
 
-        Ok(())
+    assert_eq!(obsts.len(), num_obsts);
+
+    let obsts = obsts
+        .into_iter()
+        .map(|obst| {
+            let next = next()?;
+            let rgb = opt(vec3f);
+            let (bounds_idx, color_index, block_type, rgb) = parse!(next => bounds3i i32 i32 rgb)?;
+
+            if (color_index == -3) != rgb.is_some() {
+                return Err(err(
+                    // TODO: This just passes the whole line
+                    next,
+                    ErrorKind::InvalidObstColor { color_index, rgb },
+                ));
+            }
+
+            Ok(Obst {
+                bounds: obst.bounds,
+                id: obst.id,
+                is_hole: obst.is_hole,
+                side_surfaces: obst.side_surfaces,
+                texture_origin: obst.texture_origin,
+                name: obst.name,
+                bounds_idx,
+                color_index,
+                block_type,
+                rgb,
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(obsts)
+}
+
+fn parse_vents<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
+    mut next: Src,
+) -> Result<Vec<Vent>, Error<'a>> {
+    let (num_vents, num_dummies) = parse!(next()? => usize usize)?;
+    let num_non_dummies = num_vents - num_dummies;
+
+    struct HalfVent {
+        bounds: Bounds3F,
+        vent_index: i32,
+        surface: i32,
+        texture_origin: Option<Vec3F>,
     }
+
+    let vents = repeat_n(
+        &mut next,
+        |next, i| {
+            let next = next()?;
+            let texture_origin = opt(vec3f);
+            let (bounds, vent_index, surface, texture_origin) =
+                parse!(next => bounds3f i32 i32 texture_origin)?;
+
+            if (i < num_non_dummies) != texture_origin.is_some() {
+                return Err(err(
+                    next,
+                    ErrorKind::InvalidVentTextureOrigin {
+                        i,
+                        num_vents,
+                        num_dummies,
+                        texture_origin,
+                    },
+                ));
+            }
+
+            Ok(HalfVent {
+                bounds,
+                vent_index,
+                surface,
+                texture_origin,
+            })
+        },
+        num_vents,
+    )?;
+
+    assert_eq!(vents.len(), num_vents);
+
+    let vents = vents
+        .into_iter()
+        .map(|vent| {
+            let next = next()?;
+            let rgba = opt(tuple((vec3f, f32)));
+            let (bounds_idx, color_index, draw_type, rgba) = parse!(next => bounds3i i32 i32 rgba)?;
+
+            Ok(Vent {
+                bounds: vent.bounds,
+                vent_index: vent.vent_index,
+                surface: vent.surface,
+                texture_origin: vent.texture_origin,
+                bounds_idx,
+                color_index,
+                draw_type,
+                rgba,
+            })
+        })
+        .collect::<Result<Vec<_>, Error>>()?;
+
+    Ok(vents)
+}
+
+fn parse_circular_vents<'a, Src: FnMut() -> Result<&'a str, Error<'a>>>(
+    mut next: Src,
+) -> Result<Vec<CircularVent>, Error<'a>> {
+    let num_vents = parse!(next()? => usize)?;
+
+    struct HalfCircularVent {
+        bounds: Bounds3F,
+        vent_index: i32,
+        surface: i32,
+        origin: Vec3F,
+        radius: f32,
+    }
+
+    let vents = repeat_n(
+        &mut next,
+        |next, _| {
+            let next = next()?;
+            let (bounds, vent_index, surface, origin, radius) =
+                parse!(next => bounds3f i32 i32 vec3f f32)?;
+
+            Ok(HalfCircularVent {
+                bounds,
+                vent_index,
+                surface,
+                origin,
+                radius,
+            })
+        },
+        num_vents,
+    )?;
+
+    assert_eq!(vents.len(), num_vents);
+
+    let vents = vents
+        .into_iter()
+        .map(|vent| {
+            let next = next()?;
+            let rgba = opt(tuple((vec3f, f32)));
+            let (bounds_idx, color_index, draw_type, rgba) = parse!(next => bounds3i i32 i32 rgba)?;
+
+            Ok(CircularVent {
+                bounds: vent.bounds,
+                vent_index: vent.vent_index,
+                surface: vent.surface,
+                origin: vent.origin,
+                radius: vent.radius,
+                bounds_idx,
+                color_index,
+                draw_type,
+                rgba,
+            })
+        })
+        .collect::<Result<Vec<_>, Error>>()?;
+
+    Ok(vents)
 }
