dde0782fb805d8b26236eea0aa90f5b42a9e349b
blenderfreaky <blenderfreaky@mailbox.org>
2023-03-11 04:07:38 +0100

    very rudimentary testing going over all .smv files on rescale

--- rescale-api/src/main.rs
+++ rescale-api/src/main.rs
@@ -1,3 +1,4 @@
+use fds_toolbox_core::formats::smv::Simulation;
 use rescale_api::{api::RescaleApiClient, user::files};
 
 pub mod api;
@@ -10,9 +11,20 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let files = files::list_filtered(&client, Some(".smv"), None).await?;
     dbg!(&files);
-    let file = &files.results[4];
     // dbg!(file);
-    dbg!(files::get_bytes(&client, &file.id).await?);
+    // dbg!(files::get_bytes(&client, &file.id).await?);
+
+    for file in files.results {
+        println!("-- Downloading {:?}", file.path);
+        let content = files::get_bytes(&client, &file.id).await?;
+
+        println!(" > {} bytes", content.len());
+
+        // convert content from Bytes to &str
+        let text = std::str::from_utf8(content.as_ref())?;
+
+        let sim = Simulation::parse_with_warn_stdout(text).unwrap();
+    }
 
     Ok(())
 }
