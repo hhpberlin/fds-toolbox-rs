1e5fa575ecb3d15f936fa306b58d6fbde995bb71
blenderfreaky <blenderfreaky@mailbox.org>
2023-04-10 19:37:06 +0200

    small test for moka

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,23 @@
 // use fds_toolbox_core::file::ParsedFile;
 
-fn main() {
+use moka::future::Cache;
+
+#[tokio::main]
+async fn main() {
     // let hello = ParsedFile::new(parsed, path);
+    let cache = Cache::new(100);
+
+    cache.insert(5, "Past moment").await;
+
+    dbg!(cache.get_with(5, async { "Future moment" }).await);
+
+    dbg!(cache.get_with(6, async { "Future moment" }).await);
+
+    let cc = cache.clone();
+    let f = async move { cc.get_with(7, async { "Future moment" }).await };
+    tokio::spawn(f);
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    dbg!(cache.get(&7));
 }
