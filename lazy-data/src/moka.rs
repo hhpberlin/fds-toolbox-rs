3a775e3b1f0ade6f2c1840fdd6c5e01c04545b52
blenderfreaky <blenderfreaky@mailbox.org>
2023-04-11 23:36:50 +0200

    moka cache progress

--- lazy-data/src/moka.rs
+++ lazy-data/src/moka.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashMap, sync::Arc, error::Error, io::Read, path::Path, hash::Hash};
+use std::{collections::HashMap, error::Error, hash::Hash, io::Read, path::Path, sync::Arc};
 
 use async_trait::async_trait;
-use fds_toolbox_core::{common::series::{TimeSeries0, TimeSeries2, TimeSeries3}, file::{Simulation, FileSystem, OsFs, SimulationPath}, formats::{smv, smoke::dim2::slice, csv}};
-use moka::sync::Cache;
+use fds_toolbox_core::{
+    common::series::{TimeSeries0, TimeSeries2, TimeSeries3},
+    file::{FileSystem, OsFs, ParseError, Simulation, SimulationPath},
+    formats::{csv::{self, devc::Devices, cpu::CpuData, hrr::HRRStep}, smoke::dim2::slice::{self, Slice}, smv},
+};
+use moka::future::Cache;
 use thiserror::Error;
 
 // TODO: Remove dead_code. Here for a dark cockpit.
@@ -52,7 +56,11 @@ impl FileSystem for Fs {
 
     fn file_path(&self, directory: &Self::PathRef, file_name: &str) -> Self::Path {
         match self {
-            Fs::LocalFs(fs) => fs.file_path(Path::new(directory), file_name).to_str().expect("Non-UTF8 paths are currently not supported.").to_string(),
+            Fs::LocalFs(fs) => fs
+                .file_path(Path::new(directory), file_name)
+                .to_str()
+                .expect("Non-UTF8 paths are currently not supported.")
+                .to_string(),
         }
     }
 }
@@ -70,7 +78,6 @@ impl FileSystem for Fs {
 
 // These are opaque types referencing the indicies from the perspective of the current data-layout.
 // They might be different on different runs.
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct DevcIdx(usize);
@@ -101,21 +108,45 @@ pub enum SimulationDataIdx {
 
 #[derive(Debug, Clone)]
 pub enum SimulationData {
-    Devc(Arc<TimeSeries0>),
-    Slice(Arc<TimeSeries2>),
-    Cpu(Arc<TimeSeries0>),
-    Hrr(Arc<TimeSeries0>),
+    Devc(Arc<Devices>),
+    Cpu(Arc<CpuData>),
+    Hrr(Arc<Vec<HRRStep>>),
+    Slice(Arc<Slice>),
     S3d(Arc<TimeSeries3>),
     P3d(Arc<TimeSeries3>),
 }
 
-/* 
-    ParseError<Fs::Error, smv::Error>
-    ParseError<Fs::Error, slice::Error>
-    ParseError<Fs::Error, csv::cpu::Error>
-    ParseError<Fs::Error, csv::hrr::Error>
-    ParseError<Fs::Error, csv::devc::Error>
- */
+impl SimulationData {
+    fn ref_count(&self) -> usize {
+        match self {
+            SimulationData::Devc(x) => Arc::strong_count(x),
+            SimulationData::Slice(x) => Arc::strong_count(x),
+            SimulationData::Cpu(x) => Arc::strong_count(x),
+            SimulationData::Hrr(x) => Arc::strong_count(x),
+            SimulationData::S3d(x) => Arc::strong_count(x),
+            SimulationData::P3d(x) => Arc::strong_count(x),
+        }
+    }
+
+    fn size(&self) -> usize {
+        match self {
+            SimulationData::Devc(x) => x.size_in_bytes(),
+            SimulationData::Slice(x) => x.data.size_in_bytes(),
+            SimulationData::Cpu(x) => std::mem::size_of::<CpuData>(),
+            SimulationData::Hrr(x) => x.len() * std::mem::size_of::<HRRStep>(),
+            SimulationData::S3d(x) => x.size_in_bytes(),
+            SimulationData::P3d(x) => x.size_in_bytes(),
+        }
+    }
+}
+
+/*
+   ParseError<Fs::Error, smv::Error>
+   ParseError<Fs::Error, slice::Error>
+   ParseError<Fs::Error, csv::cpu::Error>
+   ParseError<Fs::Error, csv::hrr::Error>
+   ParseError<Fs::Error, csv::devc::Error>
+*/
 
 #[derive(Debug, Error)]
 #[error(transparent)]
@@ -123,12 +154,26 @@ pub enum SimulationDataError {
     #[error("Invalid simulation key")]
     InvalidSimulationKey,
     // Io(std::io::Error),
-    Fs(FsErr),
-    Smv(smv::Error),
-    Slice(slice::Error),
-    Cpu(csv::cpu::Error),
-    Hrr(csv::hrr::Error),
-    Devc(csv::devc::Error),
+    Fs(#[from] FsErr),
+    Io(#[from] std::io::Error),
+    Smv(#[from] smv::Error),
+    Slice(#[from] slice::Error),
+    Cpu(#[from] csv::cpu::Error),
+    Hrr(#[from] csv::hrr::Error),
+    Devc(#[from] csv::devc::Error),
+}
+
+impl<ParseErr: Error> From<ParseError<FsErr, ParseErr>>
+    for SimulationDataError
+    where SimulationDataError: From<ParseErr>
+{
+    fn from(value: ParseError<FsErr, ParseErr>) -> Self {
+        match value {
+            ParseError::Fs(err) => SimulationDataError::Fs(err),
+            ParseError::Io(err) => SimulationDataError::Io(err),
+            ParseError::Parse(err) => SimulationDataError::from(err),
+        }
+    }
 }
 
 impl MokaStore {
@@ -138,25 +183,48 @@ impl MokaStore {
                 // Up to 10,000 entries.
                 .max_capacity(10_000)
                 // Create the cache.
+                .weigher(|k, v: &SimulationData| {
+                    // This is a rather arbitrary way of weighing the values.
+
+                    let s = v.size();
+
+                    // Evicting values with references just loses the value and doesn't free the memory, so we weigh them higher to prevent that.
+                    let r = v.ref_count();
+
+                    (s.ilog2() as u32) * (r as u32)
+                })
                 .build(),
             simulations: HashMap::new(),
         }
     }
 
-    pub async fn get_direct(&self, idx: SimulationsDataIdx) -> Result<SimulationData, SimulationDataError> {
-        let simulation = self.simulations.get(&idx.0).ok_or(SimulationDataError::InvalidSimulationKey)?;
+    pub async fn get_direct(
+        &self,
+        idx: SimulationsDataIdx,
+    ) -> Result<SimulationData, SimulationDataError> {
+        let simulation = self
+            .simulations
+            .get(&idx.0)
+            .ok_or(SimulationDataError::InvalidSimulationKey)?;
         match idx.1 {
             SimulationDataIdx::Devc(idx) => match simulation.csv_devc().await {
                 Ok(devc) => Ok(SimulationData::Devc(Arc::new(devc))),
-                Err(err) => Err(SimulationDataError::Devc(err)),
+                Err(err) => Err(err.into()),
             },
-            SimulationDataIdx::Slice(idx) => match simulation.slice(idx.0) {
-                
+            SimulationDataIdx::Slice(idx) => match simulation.slice(idx.0).await {
+                Ok(slice) => Ok(SimulationData::Slice(Arc::new(slice))),
+                Err(err) => Err(err.into()),
             },
             SimulationDataIdx::Cpu(idx) => todo!(),
             SimulationDataIdx::Hrr(idx) => todo!(),
             SimulationDataIdx::S3d(idx) => todo!(),
             SimulationDataIdx::P3d(idx) => todo!(),
         }
+    }
+
+    pub fn get(&self, idx: SimulationDataIdx) {
+        // self.cache.get_with(key, init)
+        // self.cache.entry(idx).or_insert_with(init)
+        // self.cache.get
     }
 }
